2ce923c101bda4d7ca872e75d26725645de85064
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-17 03:55:15 -0300

    feat: configuring Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,18 @@
 import { fastify } from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.get('/hello', () => {
-    return "Hello NLW"
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany({})
+
+    return habits
 })
 
 app.listen({
     port: 3333,
+}).then(() => {
+    console.log("HTTP Server Running! - Port 3333")
 })
