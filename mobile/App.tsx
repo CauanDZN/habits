a06b828448cd4569f3b1e4adc7be0b5d77235779
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-17 18:46:38 -0300

    feat: ensuring fonts are loaded

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { 
+  useFonts, 
+  Inter_400Regular, 
+  Inter_600SemiBold, 
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  })
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
