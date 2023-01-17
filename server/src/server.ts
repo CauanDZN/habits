2845d3de1dfd8ffce5c973c824c9ece7850ef098
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-17 03:59:18 -0300

    feat: configuring CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import { fastify } from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
-
 
 const app = fastify();
 const prisma = new PrismaClient();
+
+app.register(cors)
 
 app.get('/hello', async () => {
     const habits = await prisma.habit.findMany({})
