1b05f49df209b60de2798cefc9602e069a47b40f
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-18 01:31:47 -0300

    feat: day range generation

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,43 +1,50 @@
-import { HabitDay } from "./HabitDay"
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+import { HabitDay } from "./HabitDay";
 
 const weekDays = [
-    'D',
-    'S',
-    'T',
-    'Q',
-    'Q',
-    'S',
-    'S',
-]
+  'D',
+  'S',
+  'T',
+  'Q',
+  'Q',
+  'S',
+  'S',
+];
 
-export function SummaryTable(){
-    return (
-        <div className="w-full flex">
-            <div className="grid grid-rows-7 grid-flow-row gap-3">
-                {weekDays.map((weekDay, i) => {
-                    return (
-                        <div 
-                            key={`${weekDay}-${i}`}
-                            className="text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center"
-                        >
-                            {weekDay}
-                        </div>
-                    )
-                })}
-            </div>
+const summaryDates = generateDatesFromYearBeginning()
 
-            <div className="grid grid-rows-7 grid-flow-col gap-3">
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
+const minimumSummaryDatesSize = 18 * 7 // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
+export function SummaryTable() {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, i) => {
+          return (
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
             </div>
-        </div>
-    )
+          )
+        })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((date) => {
+          return (
+            <HabitDay key={date.toString()} />
+          )
+        })}
+
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+          return (
+            <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+          )
+        })}
+      </div>
+    </div>
+  );
 }
