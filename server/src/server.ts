201c2bbe8511da574e61e8668d4a14e99fd215c4
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-17 22:42:55 -0300

    feat: isolating files from the backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,11 @@
 import { fastify } from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './routes';
 
 const app = fastify();
-const prisma = new PrismaClient();
 
 app.register(cors)
-
-app.get('/hello', async () => {
-    const habits = await prisma.habit.findMany({})
-
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333,
